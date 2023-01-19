e472c86aff180bb7d85b477b245518c6aa682ba3
= <=>
2023-01-20 03:00:41 +0900

    404

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('common/', include('common.urls')),
     path('', include('main.urls'))
+    
 ]
+handler404 = 'common.views.page_not_found'
